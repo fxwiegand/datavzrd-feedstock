37f9c41cdfa29d127825cdd1260ba2d0fad3c997
Marcel Bargull <mbargull@users.noreply.github.com>
2024-10-04 16:44:45 +0200

    Do not set PYO3_PYTHON for cross-compile
    Maybe ${PYTHON} conflicts with cross-python's shim.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
-
-export PYO3_PYTHON="${PYTHON}"
-
+if [ "${target_platform}" = "${build_platform}" ]; then
+  export PYO3_PYTHON="${PYTHON}"
+fi
 cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
