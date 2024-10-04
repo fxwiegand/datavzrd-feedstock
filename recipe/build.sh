4d45dbeda78a89798ca7d49de671e400f1bc5407
Marcel Bargull <mbargull@users.noreply.github.com>
2024-10-04 16:03:47 +0200

    PYO3_CROSS_* variables already set via cross-python

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,5 @@
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
 
 export PYO3_PYTHON="${PYTHON}"
-if [ "${target_platform}" != "${build_platform}" ]; then
-  export PYO3_CROSS_INCLUDE_DIR="${PREFIX}/include"
-  export PYO3_CROSS_LIB_DIR="${PREFIX}/lib/python${PY_VER}"
-  export PYO3_CROSS_PYTHON_VERSION="${PY_VER}"
-fi
 
 cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
